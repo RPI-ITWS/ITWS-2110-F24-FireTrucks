54b8d9123afce003466902402e62bc9d0ea0de79
Robert Acebedo <robace747@gmail.com>
2024-10-25 11:24:21 -0400

    Trying to fix db connection

--- login/config.php
+++ login/config.php
@@ -1,5 +1,5 @@
 <?php
-   @ $db = new mysqli('localhost', 'phpmyadmin', 'Marketplace18', 'marketplace');
+   $db = new mysqli('firetrucks.eastus.cloudapp.azure.com', 'phpmyadmin', 'Marketplace18', 'marketplace');
 
    if ($db->connect_error) {
       die("Connection failed: " . $db->connect_error);
